86cc37a46912b03c561a3ea2e4fe354c4d709708
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-04-06 12:23:50 -0300

    feat: create mutation for create a new post

---
+++ src/graphql/schema/mutation-type.ts
@@ -0,0 +1,13 @@
+import { GraphQLObjectType } from "graphql";
+
+import PostMutations from '../post/mutations';
+
+
+const MutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: () => ({
+    ...PostMutations
+  })
+});
+
+export default MutationType;
